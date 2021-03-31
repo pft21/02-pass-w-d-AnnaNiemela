583b4f8c3ea5b11ec4722f4e8cbc6f7062e0b4a9
Anna Niemelä <anna.niemela@studerande.plushogskolan.se>
2021-03-31 16:06:55 +0200

    Added testcases to ResultsFromInputsTest

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -6,11 +6,33 @@ import org.junit.Test;
 
 public class ResultsFromInputsTest {
 
+	
 	@Test
-	public void testAgeGettersAndSettersMethods() {
+	public void testNameLengthGettersAndSetters() {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForNameLenght(10);
-		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
+		resultsFromInputs.setResultForNameLenght(1);
+		assertEquals("The result should be 1", resultsFromInputs.getResultForNameLenght(), 1);
+	}
+	
+	@Test
+	public void testGenderGettersAndSetters() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForGender(2);
+		assertEquals("The result should be 2", resultsFromInputs.getResultForGender(), 2);
+	}
+	
+	@Test
+	public void testAgeGettersAndSetters() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForAge(3);
+		assertEquals("The result should be 3", resultsFromInputs.getResultForAge(), 3);
+	}
+	
+	@Test
+	public void testHomecityGettersAndSetters() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForHomeCity(4);
+		assertEquals("The result should be 4", resultsFromInputs.getResultForHomeCity(), 4);
 	}
 
 }
