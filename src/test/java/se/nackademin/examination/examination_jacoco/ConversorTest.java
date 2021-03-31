e6550a1b3ecd1213d1b95a893f384c2a97040d55
Anna Niemelä <anna.niemela@studerande.plushogskolan.se>
2021-03-31 13:54:10 +0200

    Added testcases to ConversorTest

--- src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
@@ -7,10 +7,30 @@ import org.junit.Test;
 public class ConversorTest {
 
 	@Test
-	public void testConversor() {
+	public void testNameConverter() {
 		Conversor conversor = new Conversor();
 		conversor.setNameConverterArrayValues();
 		assertEquals(conversor.getNameConverterArray().get(0), "Your first name is smaller than your last name");
+	}
+	
+	@Test
+	public void testGenderConverter() {
+		Conversor conversor = new Conversor();
+		conversor.setGenderConverterArrayValues();
+		assertEquals(conversor.getGenderConverterArray().get(1), "woman!");
+	}
+	
+	@Test
+	public void testAgeConverter() {
+		Conversor conversor = new Conversor();
+		conversor.setAgeConverterArrayValues();
+		assertEquals(conversor.getAgeConverterArray().get(0), "You are young and talented! Things will come your way soon enough! ");
+	}
+	@Test
+	public void testCityConverter() {
+		Conversor conversor = new Conversor();
+		conversor.setCityConverterArrayValues();
+		assertEquals(conversor.getCityConverterArray().get(10), " a letter that comes after J, ");
 	}
 
 }
